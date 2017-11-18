7980e4978e0aaaceb224bf003114f3fa66f4f62e
Prerna <prerna16.k@gmail.com>
2017-11-18 03:34:34 -0800

    Initial commit

---
+++ ECommerceApp/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp
+{
+    class Product
+    {
+        int ProductID;
+        String ProductName;
+        String ProductDescription;
+        int ProductQuantity;
+        //List of sellers
+    }
+
+
+}
